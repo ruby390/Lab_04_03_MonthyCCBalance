b33d774bac820f3b917950983eada3d449589852
cleoc <kimuraam@mail.uc.edu>
2022-09-12 15:39:59 -0400

    project init

--- src/mothlyBalance.java
+++ src/mothlyBalance.java
@@ -1,10 +1,10 @@
 public class mothlyBalance {
         public static void main(String []args){
-            int startingBalance = 5000;
+            int startingBallance = 5000;
             double interestRate = .17;
-            double oneMonthInterest = startingBalance + interestRate;
-            double twoMonthInterest = oneMonthInterest + startingBalance + interestRate;
-            System.out.println ("Your starting credit card ballence is a 5,000");
+            double oneMonthInterest = startingBallance + interestRate;
+            double twoMonthInterest = oneMonthInterest + startingBallance + interestRate;
+            System.out.println ("Your starting credit card balance is a 5,000");
             System.out.println("Your interest after one month is $" + oneMonthInterest);
             System.out.println("Your interest after two months is $" + twoMonthInterest);
         }
